35bda2ba41714c8775047a963706eb92b7a3c9c2
Daniel Borges <danprog91@gmail.com>
2022-04-20 17:56:46 -0300

    implemented basic CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://Arievilo:9HKpCGRRal5NjIGs@crudnestjs.qu56g.mongodb.net/test',
+    ),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
